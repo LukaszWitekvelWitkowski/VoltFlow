ddc8cd097bdcd83b16522348656aee2d09b02f6e
Łukasz Witek vel Witkowski <l.witekvelwitkowski@gmail.com>
2026-02-06 02:05:43 +0100

    feat: add document management and lot allocation entities

---
+++ apps/web/VoltFlow.Service/src/VoltFlow.Service.Core/Entities/TransactionLotAllocation.cs
@@ -0,0 +1,16 @@
+﻿namespace VoltFlow.Service.Core.Entities
+{
+    public class TransactionLotAllocation
+    {
+        public int IdAllocation { get; set; }
+        public int TransactionId { get; set; }
+        public int PurchaseLotId { get; set; }
+        public int QuantityTaken { get; set; }
+        public decimal UnitNetCost { get; set; }
+
+        // Nawigacja
+        public virtual PurchaseLot PurchaseLot { get; set; } = null!;
+        // Zakładam, że masz encję Transaction
+        public virtual Transaction Transaction { get; set; } = null!;
+    }
+}
--- apps/web/VoltFlow.Service/src/VoltFlow.Service.DataBaseRefresher/SeedScripts/3_Element.sql
+++ apps/web/VoltFlow.Service/src/VoltFlow.Service.DataBaseRefresher/SeedScripts/3_Element.sql
@@ -1,4 +1,4 @@
-INSERT INTO "element" ("IdElement", "Name", "Description", "IsObsolete", "ElementGroupId") VALUES
+INSERT INTO "Element" ("IdElement", "Name", "Description", "IsObsolete", "ElementGroupId") VALUES
 (1, 'Rezystor 10k Ohm', 'Rezystor wêglowy 0.25W', false, 1),
 (2, '£o¿ysko 608-ZZ', '£o¿ysko do silników elektrycznych', false, 2),
 (3, 'Przewód miedziany 1.5mm', 'Przewód instalacyjny jedno¿y³owy', false, 3);
